8c1c9d2d2f5c9c9f935929bfa35ef60005385e61
root <root@localhost.localdomain>
2017-09-13 22:07:55 -0400

    final

---
+++ pygeo-0.1.3/pythonscript.py
@@ -0,0 +1,33 @@
+import pygeoip
+import socket 
+import pygeo 
+##from geopy.geocoders import Nominatim
+
+gi = pygeoip.GeoIP('GeoLiteCity.dat')
+
+print("IPTracker returns the GeoLocation of the given IP address or URL. Written By Suphasith Usdonvudhikai v1.0 9/10/2017")
+
+userInput = input("Enter 1 for URL; Enter 2 for IP Address Lookup: ")
+
+if userInput == 1:
+	userInput = raw_input("Enter Website URL: www.google.com\n")
+	IPv4 = socket.gethostbyname(userInput)
+
+elif userInput == 2:
+	IPv4 = raw_input("Enter IP address: xxx.xxx.xxx.xxx\n")
+
+else:
+	print("Wrong user input")
+	quit()
+
+dictionary = gi.record_by_addr(IPv4)
+##geolocator = Nominatim()
+##lat = dictionary['latitude']
+##lon = dictionary['longitude']
+##string = "\"" + str(lat) + ", " + str(lon) + "\""
+##location = geolocator.reverse("%(lat)s, %(lon)s")
+#location = geolocator.reverse("52.509669, 13.376294")
+print (dictionary)
+
+
+
